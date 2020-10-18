cc791f98e7e9118f771c9209d430284b3f1314e0
Giacomo <giacomo.tom.petrucci@tiscali.it>
2020-10-18 19:18:38 +0200

    Deleted "greetings" example

--- src/main/java/tk/gtpware/smarthomehubspring/SmartHomeHubSpringController.java
+++ src/main/java/tk/gtpware/smarthomehubspring/SmartHomeHubSpringController.java
@@ -10,12 +10,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class SmartHomeHubSpringController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
-    }
-
     @PostMapping("/WeatherStation")
     public void receiveWeatherStationData(@RequestBody WeatherStationData data){
         WeatherStation.getInstance().receiveData(data);
--- src/main/java/tk/gtpware/smarthomehubspring/WeatherStationData.java
+++ src/main/java/tk/gtpware/smarthomehubspring/WeatherStationData.java
@@ -1,6 +1,7 @@
 package tk.gtpware.smarthomehubspring;
 
 public class WeatherStationData {
+    String deviceID;    //useless for now, but might be useful in case I decide to have more stations
     float temperature;
     float humidity;
     float heatIndex;
