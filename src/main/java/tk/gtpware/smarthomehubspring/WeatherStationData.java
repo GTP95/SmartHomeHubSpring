9b943c69c5c7e2c5bc9fb834ff37908a321aff63
Giacomo <giacomo.tom.petrucci@tiscali.it>
2020-10-20 20:12:55 +0200

    Fixed another incorrectly named JSON field

--- src/main/java/tk/gtpware/smarthomehubspring/WeatherStationData.java
+++ src/main/java/tk/gtpware/smarthomehubspring/WeatherStationData.java
@@ -6,6 +6,6 @@ public class WeatherStationData {
     @JsonProperty("deviceID") String deviceID;    //useless for now, but might be useful in case I decide to have more stations
     @JsonProperty("temperature") float temperature;
     @JsonProperty("humidity") float humidity;
-    @JsonProperty("heatIndex") float heatIndex;
+    @JsonProperty("heat index") float heatIndex;
     @JsonProperty("timestamp") String lastUpdateTimestamp;
 }
